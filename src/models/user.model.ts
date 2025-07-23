52cbb817152708e91aff4c0046ac241c64209d8c
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-23 20:03:09 +0545

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      trim: true,
+      required: [true, "first name is required!"],
+    },
+    last_name: {
+      type: String,
+      trim: true,
+      required: [true, "last name is required!"],
+    },
+    role: {
+      type: Schema.Types.ObjectId,
+      ref: "role",
+    },
+  },
+  { timestamps: true }
+);
