3f4f5d67479b75889f85bae9f22f527cee951ed1
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-30 19:52:18 +0545

    removing create student

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -4,29 +4,6 @@ import { asyncHandler } from "../utils/async-handler.utils";
 import { Student } from "../models/student.model";
 import { CustomError } from "../middlewares/error-handler.middleware";
 
-//? creating student
-
-export const createStudent = asyncHandler(
-  async (req: Request, res: Response) => {
-    const { user_id, class_id, roll_number } = req.body;
-
-    const students = await Student.create({
-      user_id,
-      class_id,
-      roll_number,
-    });
-
-    await students.save();
-
-    res.status(201).json({
-      message: "Student created Successfully...",
-      status: "Success",
-      success: true,
-      data: students,
-    });
-  }
-);
-
 //? getting all students
 
 export const getAllStudent = asyncHandler(
--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import {
-  createStudent,
   getAllStudent,
   getStudentById,
 } from "../controllers/student.controller";
 
 const router = express.Router();
 
-router.post("/", createStudent);
 router.get("/", getAllStudent);
 router.get("/:id", getStudentById);
 
