2be52bc84bd00912efb2b4ffa7f76c3d13bb131d
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-06 01:00:32 +0545

    attendance model

---
+++ src/models/attendance.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+enum StudentStatus {
+  ABSENT = "ABSENT",
+  PRESENT = "PRESENT",
+  SICK_LEAVE = "SICK_LEAVE",
+  URGENT_LEAVE = "URGENT_LEAVE",
+  LATE = "LATE",
+  HALF_TIME = "HALF_TIME",
+  PUBLIC_HOLIDAY = "PUBLIC_HOLIDAY",
+}
+
+const attendanceSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  status: {
+    type: String,
+    enum: StudentStatus,
+    required: true,
+  },
+  date: Date,
+  remark: {
+    type: String,
+  },
+});
+
+export const Attendance = model("Attendance", attendanceSchema);
