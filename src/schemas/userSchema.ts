d43b77af20f3b1d5e03dfe41b140d032ba6bcef6
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-11 22:19:13 +0545

    using zod for for data validation of user

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,42 @@
+import { email, string, z } from "zod";
+import { IGender } from "../types/global.type";
+
+//  Zod Schema using nativeEnum
+export const userSchema = z.object({
+  first_name: z.string().trim().min(1, { message: "First name is required" }),
+
+  last_name: z.string().trim().min(1, { message: "Last name is required" }),
+
+  password: z
+    .string()
+    .min(5, { message: "Password must be at least 5 characters" }),
+
+  role: z.string(),
+
+  email: z.email({ message: "Invalid email format" }),
+
+  age: z.number().int().positive({ message: "Age must be a positive integer" }),
+
+  phone_number: z
+    .number()
+    .min(1000000000, { message: "Phone number seems invalid" }),
+
+  date_of_birth: z.string({
+    message: "Date of birth must be a valid date",
+  }),
+
+  address: z.string().min(1, { message: "Address is required" }),
+
+  gender: z.enum(IGender, { message: "Valid gender is required" }),
+
+  profile_image: z.object({
+    path: z.string(),
+    public_id: z.string(),
+  }),
+});
+
+type IUserSchema = z.infer<typeof userSchema>;
+
+// const zodValidate = userSchema.safeParse(req.body);
+
+// zodValidate.success;
